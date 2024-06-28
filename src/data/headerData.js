f56e655ea57ad379b1cbc210a2fcccf48235f2c7
Akashx1550 <akash1630.be21@chitkara.edu.in>
2024-06-28 18:09:08 +0530

    Added profile.png

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
-import profile from '../assets/png/profile.png'
+import Profile from '../assets/png/Profile.png'
 
 export const headerData = {
     name: 'Akash',
     title: "Web Developer",
     desciption:"In a world constantly changing, he finds the patterns others miss. Whether it's code or design, his focus is unwavering. Prepared for every challenge, he turns complexity into simplicity. Welcome to his portfolio, where creativity meets precision.",
-    image: profile,
+    image: Profile,
     resumePdf: resume
 }
